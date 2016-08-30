9159236bf06fdaecf6390378cefb1f72b69dcbad
Przemek <przem.dro@gmail.com>
2016-08-30 18:55:08 +0200

    x

--- src/projects/project1/models/model1.js
+++ src/projects/project1/models/model1.js
@@ -5,9 +5,18 @@
                 Qmode: 'addToParent',
             },
             Shapes:{
+                strokeWidth: 1,
+                strokeColor: 'black',
                 fillColor: 'none',
             }
         },
+        reddish: {
+            Shapes:{
+                strokeColor: 'red',
+                fillColor: 'red',
+                fillOpacity: 0.3,
+            }
+        }
 
     }
     Groups:{
@@ -30,33 +39,24 @@
         0:{G:'grupa1', R:90, Q:30},
         1:{G:'grupa1', R:50, Q:60},
         2:{G:'grupa1', R:90, Q:-30},
-        3:{G:'grupa1', R:50, Q:-60},
-        4:{},
-        5:{},
-        6:{},
-        7:{},
-        8:{},
-        9:{},
-        10:{},
-        11:{},
-        glut2:{},
-        12:{},
-        17:{},
-        21:{},
-        37:{},
-        glut1:{},
-        glut3:{},
+        glut:{G:'grupa1', R:50, Q:-60},
     },
     Shapes:{
         0:{
             P:[
-                ['M',0,'L',1,'L',2,'L',3,'L',0,'z'],
-                ['M',0,'Q',1,2,3],
+                [0,'L',1,'L',2,'L','glut','L',0,'z'],
+                [0,'Q',1,2,'glut'],
             ],
             strokeWidth: 3,
             strokeColor: 'red',
-            fillColor: 'none',
         },
+        one:{
+            P:[
+                [1,'L',2,'L',3,'L',1,'z']
+            ],
+            Styles:['reddish'],
+
+        }
     },
     Images:{
 
